983018c030a3df89d0cf4a6fc865307e6429b30b
Victor Todorov <117980706+viktorchet0o@users.noreply.github.com>
2024-09-13 18:42:10 +0300

    Add files via upload

---
+++ Programing fundamentals for QA/ConsoleApp1/05.Basketball.Equipment/Program.cs
@@ -0,0 +1,14 @@
+﻿//Input
+int trainingFee = int.Parse(Console.ReadLine());
+
+//Calculations
+double priceForBasketballSnikers = trainingFee * 0.6; // -40%
+double priceForBasketballUniform = priceForBasketballSnikers * 0.8; // - 20%
+double priceForBall = priceForBasketballUniform / 4;
+double priceForAccessories = priceForBall / 5;
+
+double finalPrice = trainingFee + priceForBasketballSnikers + priceForBasketballUniform + priceForBall + priceForAccessories;
+Console.WriteLine(finalPrice);
+//Output
+
+
